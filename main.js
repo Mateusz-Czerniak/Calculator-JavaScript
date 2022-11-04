eefb425415d2da665d1b05ec002ed3923c0dfbb9
Mateusz Czerniak <106752244+Mateusz-Czerniak@users.noreply.github.com>
2022-11-04 16:26:56 +0100

    commit

--- main.js
+++ main.js
@@ -1,98 +1,118 @@
-const currentNumber = document.querySelector ('.currentNumber');
+const currentNumber = document.querySelector('.currentNumber');
 
-const previousNumber = document.querySelector ('.previousNumber p');
+const previousNumber = document.querySelector('.previousNumber p');
 
-const mathSign = document.querySelector ('.mathSign');
+const mathSign = document.querySelector('.mathSign');
 
-const numbersButtons = document.querySelectorAll ('.number');
+const numbersButtons = document.querySelectorAll('.number');
 
-const operatorsButtons = document.querySelectorAll ('.operator');
+const operatorsButtons = document.querySelectorAll('.operator');
 
-const equalsButton = document.querySelector ('.equals');
+const equalsButton = document.querySelector('.equals');
 
-const clearButton = document.querySelector ('.clear');
+const clearButton = document.querySelector('.clear');
 
-const calculatorHistory = document.querySelector ('.history');
+const calculatorHistory = document.querySelector('.history');
 
-const historyBtn = document.querySelector ('.history-btn');
-
+const historyBtn = document.querySelector('.history-btn');
 
 
 let result = '';
 
 
-function displayNumbers () {
-   if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
-   if(this.textContent === '.' && currentNumber.innerHTML === '') 
-   return currentNumber.innerHTML = '.0';
 
-   currentNumber.innerHTML += this.textContent;
+
+
+
+function displayNumbers () {
+        if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
+        if(this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '.0'
+
+        currentNumber.innerHTML += this.textContent;
 }
+
+
+
 
 function operate () {
-   if(currentNumber.innerHTML === '' && this.textContent ==='-'){
-    currentNumber.innerHTML = '-'
-    return;
-   } 
+    if(currentNumber.innerHTML === '' && this.textContent ==='-'){
+        currentNumber.innerHTML = '-';
+        return;
+    }
+    
 
-   else if (currentNumber.innerHTML === '') {
-    return;
-   }
+     else if (currentNumber.innerHTML === '') {
+        return;
+     }
 
-   if(mathSign.innerHTML !== '') {
-    showResult();
-   }
-   previousNumber.innerHTML = currentNumber.innerHTML;
-   mathSign.innerHTML = this.textContent;
-   currentNumber.innerHTML = '';
+     if(mathSign.innerHTML !== '') {
+         showResult();
+     }
+     previousNumber.innerHTML = currentNumber.innerHTML;
+     mathSign.innerHTML = this.textContent;
+     currentNumber.innerHTML ='';
 }
+
+
+
 
 
 function showResult () {
-   if(previousNumber.innerHTML === '' || currentNumber.innerHTML === '') return;
+    if(previousNumber.innerHTML === '' || currentNumber.innerHTML === '') return;
 
-   let a = Number(currentNumber.innerHTML);
-   let b = Number(previousNumber.innerHTML);
-   let operator = mathSign.innerHTML;
+    let a = Number(currentNumber.innerHTML);
+    let b = Number(previousNumber.innerHTML);
+    let operator = mathSign.innerHTML;
 
-   switch (operator) {
-    case '+':
-    result= a + b;
-    break;
-    case '-':
-    result = b - a;
-    break;
-    case 'x':
-    result = a * b;
-    break
-    case ':':
-    result = b / a;   
-    break 
-    case '2^':
-    result = b ** a;
-   }
 
-   addToHistory ();
-   historyBtn.classList.add('active');
-   currentNumber.innerHTML = result;
-   previousNumber.innerHTML = '';
-   mathSign.innerHTML = '';
+    switch(operator) {
+        case '+':
+        result = a + b;
+        break;
+        case '-':
+        result = b - a;
+        break;
+        case 'x':
+        result = a * b;
+        break;
+        case ':':
+        result = b / a;
+        break;
+        case '2^':
+        result = b ** a;
+        break;
+    }
+
+    addToHistory();
+    historyBtn.classList.add('active');
+    currentNumber.innerHTML = result;
+    previousNumber.innerHTML = '';
+    mathSign.innerHTML = '';
+
 }
 
 function addToHistory () {
-    const newHistoryItem = document.createElement ('li');
-    newHistoryItem.innerHTML = `${currentNumber.innerHtml} ${matSign.innerHtml} 
-    ${previousNumber.innerHTML} = ${result}`
+    const newHistoryItem = document.createElement('li');
+    newHistoryItem.innerHTML = `${currentNumber.innerHTML} ${mathSign.innerHTML} ${previousNumber.innerHTML} = ${result}`
     newHistoryItem.classList.add('history-item');
     calculatorHistory.appendChild(newHistoryItem);
 }
 
 
-function clearScreen () {
-
-}
-
 function clearHistory () {
+    calculatorHistory.textContent = '';
+    if(calculatorHistory.textContent === '') {
+        historyBtn.classList.remove('active');
+    }
+}
+
+
+
+function clearScreen () {
+    result = '';
+    currentNumber.innerHTML = '';
+    previousNumber.innerHTML = '';
+    mathSign.innerHTML = '';
 
 }
 
@@ -103,18 +123,18 @@ function clearHistory () {
 
 
 
+ // Nasluchiwanie przyciskow
+
+ operatorsButtons.forEach((button) => button.addEventListener('click', operate))
+
+ equalsButton.addEventListener('click', showResult);
 
 
-// Nasłuchiwanie przycisków
+ clearButton.addEventListener('click', clearScreen);
 
-operatorsButtons.forEach((button) => button.addEventListener('click', operate))
+ numbersButtons.forEach((button) => {
+     button.addEventListener('click', displayNumbers)
+ })
 
-equalsButton.addEventListener ('click', showResult);
 
-clearButton.addEventListener ('click', clearScreen);
-
-numbersButtons.forEach((button) => {
-    button.addEventListener('click', displayNumbers)
-})
-
-historyBtn.addEventListener ('click', clearHistory);
+ historyBtn.addEventListener('click', clearHistory);
